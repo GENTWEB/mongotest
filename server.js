50dead0e04a9e478d7fdbc29fff618996d202075
ethan gent <ethgent@gmail.com>
2019-03-10 14:46:31 -0400

    is this working?
    trying to fix git mistakes. this is what i get for trying to use gitkraken without knowing to much about it

--- server.js
+++ server.js
@@ -11,11 +11,44 @@ var app = express();
 app.use(express.urlencoded({ extended: true}));
 //tell express to parse body with json:
 app.use(express.json());
-//tell express to use public a default
-app.use(express.static("public"));
 
+<<<<<<< Updated upstream
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI, {useNewURLPArser: true});
+=======
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/NprArts", { useNewUrlParser: true });
+
+//Function for checking db for articles that have been scraped
+app.get('/', function (req, res) {
+    res.render('index');
+  });
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.npr.org/sections/arts/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log("no new articles");
+>>>>>>> Stashed changes
 
 app.get("/scrape", function(req,res) {
     axios.get(onion).then(function(response){
@@ -33,8 +66,23 @@ app.get("/scrape", function(req,res) {
                     console.log(err);
                 });
         });
+<<<<<<< Updated upstream
         res.send("scrape complete");
     });
+=======
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
+  });
+});
+
+app.get('/articles', function (req, res, next) {
+  db.Article.find(function (err, article) {
+    if (err) return res.sendStatus(500);
+    res.render('articles', {  article : article });
+  });
+>>>>>>> Stashed changes
 });
 app.get("/articles/:id", function(req, res){
     db.article.finOne({_id: req.params.id})
